1719d3966b0b74a63ab51b6cc2c6ff81a701e510
hyunjun <hyunjun5@illinois.edu>
2023-02-01 17:19:43 -0600

    coding assignment finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FetchCodingExcersice-Andrew
+
+The code reads in a transactions.csv file within the current directory. 
+It will take one argument which is the points to be spent by the user. Once the csv is read, two data structures are going to be filled as we iterate through the rows, i.e. order and rtrn. rtrn is a dictionary which is going to be tracking the total points paid by payers and order is going to keep track of the list of transactions and will be sorted after processing the csv via a lambda function. 
+
+The code will then loop through the entire list of transactions in ascending order of timestamps, deducting total available point paid by payers and the amount of points remaining to be spent by the user. 
+
+FYI, code written and tested on python version 3.9.11.
+For some reason, i was not able to run it with "python3 mycode.py 'amount'" but with "python mycode.py 'amount'" in my environment.
---
+++ mycode.py
@@ -0,0 +1,66 @@
+import csv
+import sys
+from datetime import datetime 
+
+
+# transaction object that will be iterated through when paying for the points to spend.
+class txn:
+    def __init__(self, name, points, time):
+        self.name = name
+        self.points = points
+        self.time = time
+
+def main(argv):
+    # amount to be spent
+    amount = int(argv)
+    with open("transactions.csv", 'r') as file:
+        csvread = csv.reader(file)
+
+        # date format for sorting from oldest transactions to the latest transactions.
+        date_format = '%Y-%m-%dT%H:%M:%SZ'
+
+        # order list will contain the transactions from the given csv file
+        order = []
+
+        # dictionary that contains the total points from each of the payers 
+        rtrn = dict()
+
+        # for each transaction in the csv file
+        for t in csvread:
+            t = t[0].split(",")
+
+            current = txn(t[0].strip('"'),int(t[1]),t[2].strip('"'))
+            order.append(current)  
+
+            if current.name in rtrn:    
+                rtrn[current.name] += current.points
+            else:
+                rtrn[current.name] = current.points
+
+        #sorting the list of transactions by their timestamps with a lambda function.
+        res = sorted(order, key=lambda x: datetime.strptime(x.time, date_format))
+        
+        while res:
+            pay = res.pop(0)
+
+            # if the remaining amount to be spent is still greater,
+            # then we will just deduct the points for this particular
+            # transaction we are looking at to make sure no payer goes to negative balance.
+            if amount > pay.points:
+                amount -= pay.points
+                rtrn[pay.name] -= pay.points
+
+            else:
+                
+                rtrn[pay.name] -= amount
+                break
+
+    print(rtrn)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
